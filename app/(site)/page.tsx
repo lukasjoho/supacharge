305ec56fd24e3ceeef1af098d792aa86efb7415b
lukasjoho <social@lukashoppe.com>
2023-10-15 07:10:37 +0200

    fix: remove fetch from home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,9 @@
 import GetStarted from '@/components/pages/home/GetStarted';
 import Hero from '@/components/pages/home/Hero';
-import prisma from '@/lib/prisma';
 
 const HomePage = async () => {
-  const projects = await prisma.project.findMany();
   return (
     <div className="py-24 space-y-32">
-      <pre>{JSON.stringify(projects, null, 2)}</pre>
       <Hero />
       <GetStarted />
     </div>
