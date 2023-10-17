e8fd4a3161eb3428a2eab37b47654878fb8de7a0
lukasjoho <social@lukashoppe.com>
2023-10-17 02:45:24 +0200

    feat: add loading state to google login button

--- components/shared/LoginButton.tsx
+++ components/shared/LoginButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { cn } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 import { signIn } from 'next-auth/react';
 import React from 'react';
 import { Button, ButtonProps } from '../ui/button';
@@ -10,13 +11,22 @@ interface LoginButtonProps extends ButtonProps {
 
 const LoginButton = ({ children, ...props }: LoginButtonProps) => {
   const { className, ...rest } = props;
+
+  const [loading, setLoading] = React.useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    signIn('google');
+    return () => setLoading(false);
+  };
   return (
     <Button
-      className={cn('flex gap-1.5', className)}
       {...rest}
-      onClick={() => signIn('google')}
+      onClick={handleClick}
+      className={cn('flex gap-1.5 justify-center', className)}
     >
-      {children}
+      {loading && <Loader2 className="w-4 h-4 animate-spin" />}
+      {loading ? 'Logging in...' : children}
     </Button>
   );
 };
